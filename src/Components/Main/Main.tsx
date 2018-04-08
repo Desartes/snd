86c7354ec0adadaab9cc2caa118b14664072ec2b
Filip Kolesár <koleseri.koleseri@gmail.com>
2018-04-08 16:30:52 +0200

    add city component

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -5,12 +5,13 @@ import Logo from '../../images/logo.png';
 import './Main.css';
 import SearchBar from '../SearchBar/SearchBar';
 import IconButton from '../IconButton/IconButton';
+import InfoRectangle from '../InfoRectangle/InfoRectangle';
 
 class Main extends React.Component {
 	render() {
 		return (
 			<div className="main">
-				<Container style={{ padding: '10px' }}>
+				<Container style={{ padding: '5px' }}>
 					<Row>
 						<Col xs={12}>
 							<img id="logo" src={Logo} />
@@ -26,8 +27,13 @@ class Main extends React.Component {
 						</Col>
 					</Row>
 					<Row>
-						<Col xs={12}>
+						<Col xs={12} style={{ padding: '5px' }}>
 							<SearchBar />
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={4} md={2} lg={2} xl={1} offset={{xs: 0, sm: 0, md: 0, lg: 0}} style={{padding: '15px'}}>
+							<InfoRectangle />
 						</Col>
 					</Row>
 				</Container>
