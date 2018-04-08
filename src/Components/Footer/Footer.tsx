567d46c7fdc30292becf3e075d7d62b733df9df7
Filip Kolesár <koleseri.koleseri@gmail.com>
2018-04-08 17:33:36 +0200

    add footer component

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './Footer.css';
+import { Container } from 'react-grid-system';
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<div className="footer">
+				<Container>
+					ProgressGroup KONCEPT 2018
+				</Container>
+			</div>
+		);
+	}
+}
+
+export default Footer;
