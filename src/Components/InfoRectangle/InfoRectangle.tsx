24215c23f17de59b2d60fd431f9f8d550c28b8aa
Filip Kolesár <koleseri.koleseri@gmail.com>
2018-04-08 16:30:21 +0200

    component rename

---
+++ src/Components/InfoRectangle/InfoRectangle.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './InfoRectangle.css';
+
+class InfoRectangle extends React.Component {
+	render() {
+		return (
+			<div className="infoRectangle">
+				Poprad
+			</div>
+		);
+	}
+}
+
+export default InfoRectangle;
