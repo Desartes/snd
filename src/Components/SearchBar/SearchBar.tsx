d778bdf2f1b1f12c46bd95848b36d322aaafe743
Filip Kolesár <koleseri.koleseri@gmail.com>
2018-04-15 13:54:05 +0200

    implement calback for search onchange event

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -3,13 +3,21 @@ import { Col, Row } from 'react-grid-system';
 import SearchIcon from '../../images/icons/search.png';
 import './SearchBar.css';
 
-class SearchBar extends React.Component {
+type Props = {
+	onChange: Function;
+};
+
+class SearchBar extends React.Component<Props> {
+	constructor(props: Props) {
+		super(props);
+	}
 	render() {
+		const { onChange } = this.props;
 		return (
 			<div className="searchBar">
 				<Row>
 					<Col xs={9} sm={10}>
-						<div className="inputText">some text</div>
+						<input className="inputText" onChange={event => onChange(event)} />
 					</Col>
 					<Col xs={3} sm={2} style={{display: 'inline-block', height: '100%', verticalAlign: 'middle'}}>
 						<img className="findIcon" src={SearchIcon} />
